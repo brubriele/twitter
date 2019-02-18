52595ace7545bf57e85b9f786cb62a0bbc7cac51
Bruna G Paula <brunagabriele.paula@gmail.com>
2019-02-18 16:37:15 -0300

    primeira versao script get form, estilo e ajuste cards

--- scripts/script.js
+++ scripts/script.js
@@ -1,1 +1,22 @@
 
+function getMsgForm() {
+    const x = document.forms["tweetform"]["tweetmsg"].value;
+    document.getElementById("fisrtTwt").innerHTML = x;
+    event.preventDefault()
+  }
+
+
+
+
+// próximo passo
+
+// var newParagraph = document.createElement('p');
+// var paragraphText = document.createTextNode('Novo parágrafo.');
+// newParagraph.appendChild(paragraphText);
+
+// // Armazenamos em uma varipavel o pai e em outra variável o segundo parágrafo
+// var container = document.getElementById('container');
+// var secondParagraph = container.getElementsByTagName('p')[1];
+
+// // Agora inserimos
+// container.insertBefore(newParagraph, secondParagraph);
