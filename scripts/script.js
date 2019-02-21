6059716924ada0b628db14f5bd9c32cd3c529c3c
Bruna G Paula <brunagabriele.paula@gmail.com>
2019-02-21 13:10:50 -0300

    implementa function getdate, refatora getmsg e agrupa eventlistener

--- scripts/script.js
+++ scripts/script.js
@@ -1,17 +1,22 @@
 document.getElementById('btn').addEventListener('click', getMsgForm)
-document.getElementById('input').addEventListener('input', ableBtn)
-document.getElementById('input').addEventListener('input', contador)
-
+document.getElementById('input').addEventListener('input', ()=> {
+  ableBtn();
+  contador();
+})
 
 function getMsgForm() {
+  let horas = document.createElement('p');
+  horas.innerHTML = getDate()
   const novaMensagem = document.forms["tweetform"]["tweetmsg"].value;
   let tweet = document.createElement('p');
-  let text = document.createTextNode(novaMensagem);
-  tweet.appendChild(text);
+  tweet.innerHTML = novaMensagem;
+  document.getElementById('mural').appendChild(horas);
   document.getElementById('mural').appendChild(tweet);
   document.forms["tweetform"]["tweetmsg"].value = '';
   event.preventDefault()
+  btn.disabled = true;
 }
+
 
 
 function ableBtn() {
@@ -49,49 +54,22 @@ function ableBtn() {
   }
 
 
-// Tentar implementar clone origem da div
-
-// // Get the last <li> element ("Milk") of <ul> with id="myList2"
-// var formOrigem = document.getElementById("formOrigin").firstChild;
-
-// // Copy the <li> element and its child nodes
-// var cln = formOrigem.cloneNode(true);
-
-// // Append the cloned <li> element to <ul> with id="myList1"
-// document.getElementById("formOrigin").replaceChild(cln);
-
-
-
-//   Se você passar os 140 caracteres, desative o botão.
-// Se você passar os 120 caracteres, mostre o contador com outra cor.
-// Se você passar os 130 caracteres, mostre o contador com outra cor.
-// Se você passar os 140 caracteres, mostre o contador em negativo.
-
-// próximo passo
-
-// var newParagraph = document.createElement('p');
-// var paragraphText = document.createTextNode('Novo parágrafo.');
-// newParagraph.appendChild(paragraphText);
-
-// // Armazenamos em uma varipavel o pai e em outra variável o segundo parágrafo
-// var container = document.getElementById('container');
-// var secondParagraph = container.getElementsByTagName('p')[1];
-
-// // Agora inserimos
-// container.insertBefore(newParagraph, secondParagraph);
-
-// function myFunction() {
-//     var x = document.createElement("INPUT");
-//     x.setAttribute("type", "text");
-//     x.setAttribute("value", "Hello World!");
-//     document.body.appendChild(x);
-//   }
-
-
-// contador
-
-// function myFunction() {
-//   var x = document.getElementById("input").value;
-//   document.getElementById("contador").innerHTML = 144 - x;
-// }
+  function getDate(){
+    let date = new Date;
+    let horas = date.getHours();
+    let min = date.getMinutes();
+    let horaFinal = horas + ":" + min;
+    // console.log(horaFinal)
+    
+    if (horas < 10){
+      horaFinal = horas + '0' + ":" + min;
+    }
+    else if (min < 10) {
+      horaFinal = horas + ":" + '0' + min;
+    }
+    else if (horas < 10 && min < 10){
+      horaFinal = horas + '0' + ":" + '0' + min;
+    }
+    return horaFinal
+    }
 
